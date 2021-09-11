86d87367ed6c28a0f6f1eb8fe57e478579784a69
Zoia Ponomareva <zojapon@yandex.ru>
2021-09-12 00:51:13 +0300

    Небольшие изменения

--- week19/task4/index.js
+++ week19/task4/index.js
@@ -26,20 +26,16 @@ console.log(validator.isDate("12.09.2021"));
 
 class ValidatorStatic {
     static isEmail(email) {
-        this.email = email;
         return /^([a-z0-9_-]+\.)*[a-z0-9_-]+@[a-z0-9_-]+(\.[a-z0-9_-]+)*\.[a-z]{2,6}$/i.test(email);
     }
     static isDomain(domain) {
-        this.domain = domain;
-        return /[-a-zA-Z0-9@:%_\+.~#?&//=]{2,256}\.[a-z]{2,4}\b(\/[-a-zA-Z0-9@:%_\+.~#?&//=]*)?/gi.test(this.domain);
+        return /[-a-zA-Z0-9@:%_\+.~#?&//=]{2,256}\.[a-z]{2,4}\b(\/[-a-zA-Z0-9@:%_\+.~#?&//=]*)?/gi.test(domain);
     }
     static isDate(date) {
-        this.date = date;
-        return /^\d{1,2}\.\d{1,2}\.\d{4}$/.test(this.date);
+        return /^\d{1,2}\.\d{1,2}\.\d{4}$/.test(date);
     }
     static isPhone(phone) {
-        this.phone = phone;
-        return /^((\+?7|8)[ \-] ?)?((\(\d{3}\))|(\d{3}))?([ \-])?(\d{3}[\- ]?\d{2}[\- ]?\d{2})$/.test(this.phone);
+        return /^((\+?7|8)[ \-] ?)?((\(\d{3}\))|(\d{3}))?([ \-])?(\d{3}[\- ]?\d{2}[\- ]?\d{2})$/.test(phone);
     }
 
 }
