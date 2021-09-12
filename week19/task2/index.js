b76f3f780dbc32017bd4186e564d4498ae451b5b
Zoia Ponomareva <zojapon@yandex.ru>
2021-09-12 20:34:27 +0300

    Небодбшие изменения

--- week19/task2/index.js
+++ week19/task2/index.js
@@ -22,7 +22,7 @@ function showPetData() {
 
     function checkChosenSex() {
         let theGroup = document.querySelectorAll("input[name='sex']");
-        for (let i = 0; i < theGroup.length; i++) {
+        for (i = 0; i < theGroup.length; i++) {
             if (theGroup[i].checked) {
                 break;
             }
