e8f5b006ec4a554b76f225980551bd1d52819f0d
Zoia Ponomareva <zojapon@yandex.ru>
2021-09-24 21:21:27 +0300

    Добавила количество в 1 задание

--- week21/task1/index.js
+++ week21/task1/index.js
@@ -1,9 +1,10 @@
 function showCards() {
 
     let searchTag = document.querySelector(".main__input").value;
+    let imgAmount = document.querySelector("#img-amount").value;
 if(searchTag != ""){
 
-    fetch(`http://api.giphy.com/v1/gifs/search?q=${searchTag}&api_key=vnduHrCn5R6LunjXtJM7YlPg7k06pG5P&limit=5`)
+    fetch(`http://api.giphy.com/v1/gifs/search?q=${searchTag}&api_key=vnduHrCn5R6LunjXtJM7YlPg7k06pG5P&limit=${imgAmount}`)
         .then(response => response.json())
         .then(cards => {
             console.log(cards);
@@ -15,14 +16,15 @@ if(searchTag != ""){
             if (cards.data.length === 0 || cards === undefined || cards.data === undefined ) {
                 document.querySelector(".main-container").innerHTML = "Попробуйте изменить запрос";
             } else {
+                document.querySelector(".main-container").innerHTML = "";
                 for (let i = 0; i < cards.data.length; i++) {
                     if (cards.data[i] === undefined) {
-                        document.querySelectorAll(".main__little-container")[i].innerHTML = "";
+                        document.querySelector(".main-container").innerHTML = "";
                     } else {
                         console.log(cards.data[i].images.original.url);
-                        document.querySelectorAll(".main__little-container")[i].innerHTML = "";
-                        document.querySelectorAll(".main__little-container")[i].innerHTML += `<img src="${cards.data[i].images.original.url}" alt="${i+1} картинка" class="main__img"><br/>
-                        <a href="${cards.data[i].images.original.url}" class="main__link">Ссылка на ${i+1} картинку</a><br/>`;
+                        
+                        document.querySelector(".main-container").innerHTML += `<div class="main__little-container"><img src="${cards.data[i].images.original.url}" alt="${i+1} картинка" class="main__img"><br/>
+                        <a href="${cards.data[i].images.original.url}" class="main__link">Ссылка на ${i+1} картинку</a></div>`;
                     }
                 }
             }
