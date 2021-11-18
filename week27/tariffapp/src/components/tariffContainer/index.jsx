163407c947daba4b9a03f7b18016483c3af082e0
Zoia Ponomareva <zojapon@yandex.ru>
2021-11-18 16:14:59 +0300

    Изменила функцию для клика по карточке

--- week27/tariffapp/src/components/tariffContainer/index.jsx
+++ week27/tariffapp/src/components/tariffContainer/index.jsx
@@ -1,33 +1,35 @@
-import React from "react";
+import React, { useState } from "react";
 import "./tariffContainer.scss";
 import Tariff from "../tariffs";
 import tariffs from "../data";
 import classnames from "classnames";
 
 function TariffContainer() {
-  let className;
+  const [selectedCardIndex, setSelectedCardIndex] = useState(2);
+  const handleClick = id => {
+    setSelectedCardIndex(id);
+  };
 
   return (
     <React.Fragment>
       {tariffs.map((card, i) => (
         <Tariff
           key={`${card.id}-${i}`}
-          className2={
-            (className = classnames("card-header", {
-              blue: i === 0,
-              green: i === 1,
-              red: i === 2,
-              grey: i === 3,
-            }))
-          }
-          className3={
-            (className = classnames("", {
-              cardInfo_blue: i === 0,
-              cardInfo_green: i === 1,
-              cardInfo_red: i === 2,
-              cardInfo_grey: i === 3,
-            }))
-          }
+          onClick={() => handleClick(i)}
+          isSelected={i === selectedCardIndex}
+          // isSelected={i === 2}
+          className2={classnames("card-header", {
+            blue: i === 0,
+            green: i === 1,
+            red: i === 2,
+            grey: i === 3,
+          })}
+          className3={classnames("", {
+            cardInfo_blue: i === 0,
+            cardInfo_green: i === 1,
+            cardInfo_red: i === 2,
+            cardInfo_grey: i === 3,
+          })}
           currency={card.currency}
           amount={card.amount}
           period={card.period}
@@ -35,7 +37,6 @@ function TariffContainer() {
           name={card.name}
           speed={card.speed}
           option={card.option}
-          isSelected={i === 2}
         />
       ))}
     </React.Fragment>
--- week27/tariffapp/src/components/tariffs/index.jsx
+++ week27/tariffapp/src/components/tariffs/index.jsx
@@ -1,27 +1,28 @@
 import "./tariff.scss";
-import { useState } from "react";
+// import { useState } from "react";
 import CardBigCell from "../cardBigCell";
 import CardCell from "../cardCell";
+// import classnames from "classnames";
 
 function Tariffs(props) {
-  // let className;
-  let className1 = "";
+  // const className1 = "";
 
-  const [isSelected, setSelected] = useState(props.isSelected || false);
+  // const [isSelected, setSelected] = useState(props.isSelected || false);
 
-  const handleChange = () => {
-    setSelected(!isSelected);
-  };
-  {
-    isSelected ? (className1 = "selected") : "";
-  }
+  // const handleChange = () => {
+  //   setSelected(!isSelected);
+  // };
+  // {
+  //   isSelected ? (className1 = "selected") : "";
+  // }
   return (
     <div
-      className={`card ${className1}`}
+      className={`card${props.isSelected ? " selected" : ""}`}
       id={props.id}
-      selected={props.isSelected}
-      onClick={handleChange}
+      // selected={props.isSelected}
+      // onClick={handleChange}
       // onMouseOver={handleChange}
+      onClick={props.onClick}
     >
       <CardCell className={props.className2} text={props.name} />
       <CardBigCell
